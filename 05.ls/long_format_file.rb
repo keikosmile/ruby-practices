3ab6f8f0aa2dc2d677d04762ea5c8b0bfbfd5004
keikosmile <69710419+keikosmile@users.noreply.github.com>
2021-03-16 20:02:57 +0900

    rubocopで行数超過・複雑さ以外の警告を解決

--- 05.ls/long_format_file.rb
+++ 05.ls/long_format_file.rb
@@ -1,22 +1,77 @@
+# 文字列をimmutable（破壊的変更不可）にする
+# frozen_string_literal: true
+
 require 'etc'
 
 class LongFormatFile
   attr_reader :nlink, :username, :groupname, :size, :blocks
+
   def initialize(dir, file)
-    fs = File::stat(dir + file)
+    fs = File.stat(dir + file)
     # ファイルモードを８進文字列に変換し、文字列を得る
     @mode = get_mode_string(fs.mode.to_s(8))
     @nlink = fs.nlink.to_s
     @username = Etc.getpwuid(fs.uid).name
     @groupname = Etc.getgrgid(fs.gid).name
     @size = fs.size.to_s
-    @mtime = fs.mtime.strftime("%_m %_d %H:%M")
-    if dir.empty?
-      @basename = file
-    else
-      @basename = File.basename(file)
-    end
+    @mtime = fs.mtime.strftime('%_m %_d %H:%M')
+    @basename = if dir.empty?
+                  file
+                else
+                  File.basename(file)
+                end
     @blocks = fs.blocks
+  end
+
+  # ファイルタイプ部分の文字列を得る
+  def get_file_type_string(mode_string)
+    case mode_string
+    when '010'
+      'p'
+    when '020'
+      'c'
+    when '040'
+      'd'
+    when '060'
+      'b'
+    when '100'
+      '-'
+    when '120'
+      'l'
+    when '140'
+      's'
+    else
+      '?'
+    end
+  end
+
+  # パーミッション部分の文字列を得る
+  def get_permission_string(mode_string)
+    case mode_string
+    when '7'
+      'rwx'
+    when '6'
+      'rw-'
+    when '5'
+      'r-x'
+    when '4'
+      'r--'
+    when '3'
+      '-wx'
+    when '2'
+      '-w-'
+    when '1'
+      '--x'
+    else
+      '---'
+    end
+  end
+
+  # ファイルモードの文字列を得る
+  def get_mode_string(mode_string)
+    # mode文字列を長さ7の文字列に右詰し、０でpaddingを詰める
+    mode = mode_string.rjust(6, '0')
+    "#{get_file_type_string(mode[0, 3])}#{get_permission_string(mode[3])}#{get_permission_string(mode[4])}#{get_permission_string(mode[5])}"
   end
 
   # 最高幅に合わせて、右寄せ・左寄せする
@@ -27,40 +82,8 @@ class LongFormatFile
     @size = @size.rjust(max_size)
   end
 
-  # 最終的に表示する文字列を得る
-  def get_string
-    string = @mode + "  " + @nlink + " " + @username + "  " + @groupname + "  " + @size + " " + @mtime + " " + @basename
-  end
-
-  # ファイルモードの文字列を得る
-  def get_mode_string(mode_string)
-    # mode文字列を長さ7の文字列に右詰し、０でpaddingを詰める
-    mode = mode_string.rjust(6, "0")
-    string = ""
-    # ファイルタイプ部分の文字列を得る
-    string += (
-      mode[0, 3].eql?("010") ? "p" :
-      mode[0, 3].eql?("020") ? "c" :
-      mode[0, 3].eql?("040") ? "d" :
-      mode[0, 3].eql?("060") ? "b" :
-      mode[0, 3].eql?("100") ? "-" :
-      mode[0, 3].eql?("120") ? "l" :
-      mode[0, 3].eql?("140") ? "s" : "?")
-    # パーミッション部分の文字列を得る
-    string += get_permission_string(mode[3])
-    string += get_permission_string(mode[4])
-    string += get_permission_string(mode[5])
-    string
-  end
-
-  # パーミッション部分の文字列を得る
-  def get_permission_string(mode_string)
-    mode_string.eql?("7") ? "rwx" :
-    mode_string.eql?("6") ? "rw-" :
-    mode_string.eql?("5") ? "r-x" :
-    mode_string.eql?("4") ? "r--" :
-    mode_string.eql?("3") ? "-wx" :
-    mode_string.eql?("2") ? "-w-" :
-    mode_string.eql?("1") ? "--x" : "---"
+  # ロングフォーマットファイル形式の文字列を得る
+  def long_format_file
+    "#{@mode}  #{@nlink} #{@username}  #{@groupname}  #{@size} #{@mtime} #{@basename}"
   end
 end
--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -9,214 +9,74 @@ require_relative 'long_format_file'
 
 class Ls
   def initialize
-    @option_hash = {:a=>false, :l=>false, :r=>false}
-    @wildcard_directory_array = []
+    @option_hash = { a: false, l: false, r: false }
+    @wildcard_file_directory_array = []
     @not_match_wildcard_array = []
     @not_match_file_array = []
     @wildcard_file_array = []
     @directory_array = []
-    @answer_string = ""
+    @answer_string = ''
     @console_width = IO.console_size.last
     @tabsize = 8
   end
 
   def argv_parse(argv_array)
     opt = OptionParser.new
-    opt.on('-a') {|v| v}
-    opt.on('-l') {|v| v}
-    opt.on('-r') {|v| v}
+    opt.on('-a') { |v| v }
+    opt.on('-l') { |v| v }
+    opt.on('-r') { |v| v }
     opt.order!(argv_array, into: @option_hash)
-    @wildcard_directory_array = argv_array
+    @wildcard_file_directory_array = argv_array
   end
 
-  def separate_directory_wildcard_array
+  # オプション以外のコマンドライン引数を、ディレクトリ・ワイルドカード・ファイルの配列に分ける
+  def separate_wildcard_file_directory_array
     # オプション以外の配列は空か
-    if @wildcard_directory_array.empty?
-      @directory_array.push("./")
+    if @wildcard_file_directory_array.empty?
+      @directory_array.push('./')
     else
-      @wildcard_directory_array.each do |i|
-        # ディレクトリ名か
+      @wildcard_file_directory_array.each do |i|
+        # ディレクトリなら
         if FileTest.directory?(i)
           @directory_array.push(i)
-        # ワイルドカードか
-        elsif i.include?("*") || i.include?("?") || (i.include?("[") && i.include?("]")) || (i.include?("{") && i.include?("}")) || i.include?("**/")
+        # ワイルドカードなら
+        elsif i.include?('*') || i.include?('?') || (i.include?('[') && i.include?(']')) || (i.include?('{') && i.include?('}')) || i.include?('**/')
           file_array = Dir.glob(i)
-          # ワイルドカードがマッチしなければ
+          # ワイルドカードがマッチしないなら
           if file_array.empty?
             @not_match_wildcard_array.push(i)
-          # ワイルドカードがマッチすれば
+          # ワイルドカードがマッチするなら
           else
             @wildcard_file_array.push(file_array)
           end
         # ファイルなら
+        elsif FileTest.exist?(i)
+          @wildcard_file_array.push(i)
+        # 存在しないファイルかディレクトリなら
         else
-          if FileTest.exist?(i)
-            @wildcard_file_array.push(i)
-          else
-            @not_match_file_array.push(i)
-          end
+          @not_match_file_array.push(i)
         end
       end
     end
   end
 
-  def ls
-    separate_directory_wildcard_array
-
-    # マッチしないワイルドカードがあれば、それを表示し終了
-    unless @not_match_wildcard_array.empty?
-      @answer_string = "zsh: no matches found: #{@not_match_wildcard_array[0]}\n"
-      #p @answer_string
-      print @answer_string
-      return @answer_string
-    end
-
-    # マッチしないファイル名があれば、それを表示
-    unless @not_match_file_array.empty?
-      @not_match_file_array.sort!
-      @not_match_file_array.each do |f|
-        # 提出時に\nを\tにして横並びにし、最後に\nを入れる
-        @answer_string += "ls: #{f}: No such file or directory\n"
-      end
-    end
-
-    # マッチするワイルドカードかファイル名があれば、それらを１行で表示
-    unless @wildcard_file_array.empty?
-      # 配列を一重にする
-      @wildcard_file_array.flatten!
-      # オプションを適用する
-      file_array = apply_options(@wildcard_file_array, "")
-      if @option_hash[:l]
-        @answer_string += file_array.join("\n") + "\n"
-      else
-        get_answer_string(file_array)
-      end
-    end
-
-    # ディレクトリ名配列があれば、それらを表示
-    unless @directory_array.empty?
-      # ディレクトリ名配列をソートする
-      @directory_array.sort!
-      # rオプションがあれば、ディレクトリ名配列を逆順にする
-      if @option_hash[:r]
-        @directory_array.reverse!
-      end
-      unless @wildcard_file_array.empty?
-        @answer_string += "\n"
-      end
-
-      @directory_array.each_with_index do |dir, idx|
-        if @directory_array.size > 1 || !@not_match_file_array.empty? || !@wildcard_file_array.empty?
-          @answer_string += "#{dir}:\n"
-        end
-        # ディレクトリ内のディレクトリかファイルを配列で得る
-        directory_file_array = get_directory_file_array(dir)
-        # ディレクトリかファイルの配列にオプションを適用する
-        directory_file_array = apply_options(directory_file_array, dir)
-        if @option_hash[:l]
-          @answer_string += directory_file_array.join("\n") + "\n"
-        else
-          get_answer_string(directory_file_array)
-        end
-        if @directory_array.size - 1 != idx
-          @answer_string += "\n"
-        end
-      end
-
-    end
-
-    #p @answer_string
-    print @answer_string
-    @answer_string
-  end
-
-  # ASCII文字を1文字、非ASCII文字（マルチバイト文字）を2文字としてカウントし、文字列の長さを返す
-  def ascii_width(string)
-    string.length + string.chars.reject(&:ascii_only?).length
-  end
-
-  # 表示用の文字列を得る
-  def get_answer_string(directory_file_array)
-    # 最長要素の長さ + \t = １列の長さ
-    max_directory_file = 0
-    directory_file_array.each do |directory_file|
-      tab_times = ascii_width(directory_file) / @tabsize
-      directory_file_tab_length = @tabsize * (tab_times + 1)
-      max_directory_file = ( (directory_file_tab_length > max_directory_file) ? directory_file_tab_length : max_directory_file)
-    end
-
-    # @console_width / １列の長さ = 列数
-    columns = @console_width / max_directory_file
-    columns = (directory_file_array.count < columns ? directory_file_array.count : columns)
-
-    # 要素数 / 列数 = 行数 (余がでたら繰り上がる)
-    rows = directory_file_array.count / columns
-    modulo = directory_file_array.count % columns
-    rows += (modulo == 0 ? 0 : 1)
-
-    # 最小限の行数になった上で、改めて列数を計算
-    columns = directory_file_array.count / rows
-
-    directory_file_array.each do |directory_file|
-      remainder = max_directory_file - ascii_width(directory_file)
-      tab_times = remainder / @tabsize
-      tab_remainder = remainder % @tabsize
-      if tab_remainder != 0
-        tab_times += 1
-      end
-      tmp = directory_file + "\t" * tab_times
-      directory_file.replace(tmp)
-    end
-
-    r = 0
-    while r < rows
-      answer_array = []
-      c = 0
-      while c <= columns
-        directory_file = directory_file_array[r + rows * c]
-        unless directory_file.nil?
-          answer_array.push(directory_file)
-        end
-        c += 1
-      end
-      # 最後にpushした要素の\t\t..を\nに変換する
-      last = answer_array.last.delete!("\t")
-      answer_array.last.replace(last + "\n")
-      @answer_string += answer_array.join
-      r += 1
-    end
-  end
-
-  # ディレクトリ内のディレクトリかファイルを配列で得る
-  def get_directory_file_array(dir)
-    directory_file_array = []
-    Dir.foreach(dir) do |directory_file_string|
-      directory_file_array.push(directory_file_string)
-    end
-    directory_file_array
-  end
-
-  # ファイルの配列にオプションを適用する
+  # ファイルかディレクトリの配列にオプションを適用し、適用後の配列を返す
   def apply_options(directory_file_array, dir)
     # . ./ .. .xx ./.xx ../.xx ../../../.xx などを除かずに残す
     unless @option_hash[:a]
       directory_file_array.delete_if do |f|
-        f.eql?(".") || f.eql?("./") || f.eql?("..") || File.basename(f).start_with?(".")
+        f.eql?('.') || f.eql?('./') || f.eql?('..') || File.basename(f).start_with?('.')
       end
     end
 
     directory_file_array.sort!
-    if @option_hash[:r]
-      directory_file_array.reverse!
-    end
+    directory_file_array.reverse! if @option_hash[:r]
 
     if @option_hash[:l]
       long_format_file_array = []
       # ロングフォーマット形式ファイルのインスタンスを作成し、配列に挿入する
       directory_file_array.each do |file|
-        if dir == "."
-          dir = "./"
-        end
+        dir = './' if dir == '.'
         long_format_file_array.push(LongFormatFile.new(dir, file))
       end
 
@@ -230,20 +90,143 @@ class Ls
         total += long.blocks
       end
 
-      unless dir.empty?
-        @answer_string += "total #{total}\n"
-      end
+      @answer_string += "total #{total}\n" unless dir.empty?
 
       # 最高幅に合わせて、右寄せ・左寄せする
       long_format_file_array.each do |long|
         long.set_string_width(max_nlink, max_username, max_groupname, max_size)
       end
 
+      # ロングフォーマットファイル形式の文字列を得て、入れ替える
       directory_file_array.each_with_index do |file, idx|
-        file.replace(long_format_file_array[idx].get_string)
+        file.replace(long_format_file_array[idx].long_format_file)
       end
     end
     directory_file_array
+  end
+
+  # ASCII文字を1文字、非ASCII文字（マルチバイト文字）を2文字としてカウントし、文字列の長さを返す
+  def ascii_width(string)
+    string.length + string.chars.count { |c| !c.ascii_only? }
+  end
+
+  # 画面の大きさに合わせてディレクトリかファイルを列表示するよう文字列（@answer_string)を設定する
+  def get_answer_string(directory_file_array)
+    # 最長要素の長さ + \t = １列の長さ
+    max_directory_file = 0
+    directory_file_array.each do |directory_file|
+      tab_times = ascii_width(directory_file) / @tabsize
+      directory_file_tab_length = @tabsize * (tab_times + 1)
+      max_directory_file = directory_file_tab_length > max_directory_file ? directory_file_tab_length : max_directory_file
+    end
+
+    # @console_width / １列の長さ = 列数
+    columns = @console_width / max_directory_file
+    columns = (directory_file_array.count < columns ? directory_file_array.count : columns)
+
+    # 要素数 / 列数 = 行数 (余がでたら繰り上がる)
+    rows = directory_file_array.count / columns
+    modulo = directory_file_array.count % columns
+    rows += (modulo.zero? ? 0 : 1)
+
+    # 最小限の行数になった上で、改めて列数を計算
+    columns = directory_file_array.count / rows
+
+    directory_file_array.each do |directory_file|
+      remainder = max_directory_file - ascii_width(directory_file)
+      tab_times = remainder / @tabsize
+      tab_remainder = remainder % @tabsize
+      tab_times += 1 if tab_remainder != 0
+      tmp = directory_file + "\t" * tab_times
+      directory_file.replace(tmp)
+    end
+
+    r = 0
+    while r < rows
+      answer_array = []
+      c = 0
+      while c <= columns
+        directory_file = directory_file_array[r + rows * c]
+        answer_array.push(directory_file) unless directory_file.nil?
+        c += 1
+      end
+      # 最後にpushした要素の\t\t..を\nに変換する
+      last = answer_array.last.delete!("\t")
+      answer_array.last.replace("#{last}\n")
+      @answer_string += answer_array.join
+      r += 1
+    end
+  end
+
+  # ディレクトリ内のディレクトリかファイルを配列で得て返す
+  def get_directory_file_array(dir)
+    directory_file_array = []
+    Dir.foreach(dir) do |directory_file_string|
+      directory_file_array.push(directory_file_string)
+    end
+    directory_file_array
+  end
+
+  def ls
+    # オプション以外のコマンドライン引数を、ワイルドカード・ファイル・ディレクトリの配列に分ける
+    separate_wildcard_file_directory_array
+
+    # マッチしないワイルドカードがあれば、それを表示し終了
+    unless @not_match_wildcard_array.empty?
+      @answer_string = "zsh: no matches found: #{@not_match_wildcard_array[0]}\n"
+      print @answer_string
+      return @answer_string
+    end
+
+    # マッチしないファイルかディレクトリがあれば、それを表示
+    unless @not_match_file_array.empty?
+      @not_match_file_array.sort!
+      @not_match_file_array.each do |file|
+        @answer_string += "ls: #{file}: No such file or directory\n"
+      end
+    end
+
+    # マッチするワイルドカードかファイルがあれば、それらを１行で表示
+    unless @wildcard_file_array.empty?
+      # 配列を一重にする
+      @wildcard_file_array.flatten!
+      # ファイルの配列にオプションを適用し、適用後の配列を返す
+      file_array = apply_options(@wildcard_file_array, '')
+      # lオプションがあれば、ファイルごとに１行で表示するよう文字列(@answer_string)を設定する
+      if @option_hash[:l]
+        @answer_string += "#{file_array.join("\n")}\n"
+      else
+        # lオプションがなければ、画面の大きさに合わせてファイルを列表示するよう文字列（@answer_string)を設定する
+        get_answer_string(file_array)
+      end
+    end
+
+    # ディレクトリの配列があれば、それらを表示
+    unless @directory_array.empty?
+      # ディレクトリの配列をソートする
+      @directory_array.sort!
+      # rオプションがあれば、ディレクトリ名配列を逆順にする
+      @directory_array.reverse! if @option_hash[:r]
+      @answer_string += "\n" unless @wildcard_file_array.empty?
+      @directory_array.each_with_index do |dir, idx|
+        # ディレクトリの配列が１つより多いか、マッチしないまたはマッチするファイルがあれば、ディレクトリ名を表示する
+        @answer_string += "#{dir}:\n" if @directory_array.size > 1 || !@not_match_file_array.empty? || !@wildcard_file_array.empty?
+        # ディレクトリ内のディレクトリかファイルを配列で得て返す
+        directory_file_array = get_directory_file_array(dir)
+        # ディレクトリかファイルの配列にオプションを適用し、適用後の配列を返す
+        directory_file_array = apply_options(directory_file_array, dir)
+        # lオプションがあれば、ディレクトリかファイルごとに１行で表示するよう文字列(@answer_string)を設定する
+        if @option_hash[:l]
+          @answer_string += "#{directory_file_array.join("\n")}\n"
+        # lオプションがなければ、画面の大きさに合わせてディレクトリかファイルを列表示するよう文字列（@answer_string)を設定する
+        else
+          get_answer_string(directory_file_array)
+        end
+        @answer_string += "\n" if @directory_array.size - 1 != idx
+      end
+    end
+    print @answer_string
+    @answer_string
   end
 end
 
