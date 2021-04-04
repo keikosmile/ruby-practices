7983c8503385f5bb433c9f19e6f95887d7f1aac4
keikosmile <69710419+keikosmile@users.noreply.github.com>
2021-04-04 19:01:23 +0900

    wオプションに対応、オプションの振り分け修正、無効なオプションに対応、複数ファイル（stdinなし）に対応

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -6,84 +6,122 @@
 require 'optparse'
 
 class Wc
-  def initialize(argv_array)
-    # ファイルの行数、単語数、バイト数を標準出力に表示する
-    @option_hash = { l: false, w: false, c: false }
-    @files = argv_parse(argv_array)
-    @buf = ""
+  def initialize
+    @option_hash = { l: false, w: false, c: false, m: false }
+    @files = []
+    @answer_string = ""
+    @total_hash = {l: 0, w: 0, c: 0, m: 0 }
   end
 
-  # コマンドライン引数を受け取り、オプションはハッシュに格納し、残りは配列で返す
+  def wc(argv_array)
+    # コマンドライン引数をオプションとファイル配列にパースする
+    argv_parse(argv_array)
+    # 無効なオプションが指定されていなければ
+    if @answer_string.empty?
+      total = 0
+      # ファイルごとに
+      @files.each do |file|
+        # ファイルを開いて読み込み、オプションを適用し答えの文字列を得る
+        @answer_string += " #{file}\n" if count_file(file)
+        total += 1
+      end
+      if total > 1
+        @answer_string += @total_hash[:l].to_s.rjust(8) if @option_hash[:l]
+        @answer_string += @total_hash[:w].to_s.rjust(8) if @option_hash[:w]
+        @answer_string += @total_hash[:c].to_s.rjust(8) if @option_hash[:c]
+        @answer_string += @total_hash[:m].to_s.rjust(8) if @option_hash[:m]
+        @answer_string += " total\n"
+      end
+    end
+    # 答えの文字列を表示し返す
+    print @answer_string
+    @answer_string
+  end
+
+  # コマンドライン引数を受け取り、オプションはハッシュに格納し、残りは配列に入れ空文字列を返す
   def argv_parse(argv_array)
     # OptionParseオブジェクトoptを生成する
     opt = OptionParser.new
-    # オプションを取り扱うブロックをoptに登録する
-    opt.on('-l') { |v| v }
-    opt.on('-w') { |v| v }
-    opt.on('-c') { |v| v }
-    # オプションをハッシュに格納し、残りのコマンドライン引数を配列で返す
-    opt.order!(argv_array, into: @option_hash)
-  end
-
-  def wc
-    # オプションが指定されていない場合、デフォルト
-    if !@option_hash[:l] && !@option_hash[:w] && !@option_hash[:c]
-      @option_hash = { l: true, w: true, c: true }
+    # オプションを取り扱うブロックをoptに登録し、ハッシュに格納する
+    opt.on('-l') { |v| @option_hash[:l] = true }
+    opt.on('-w') { |v| @option_hash[:w] = true }
+    opt.on('-c') { |v| @option_hash[:c] = true, @option_hash[:m] = false }
+    opt.on('-m') { |v| @option_hash[:m] = true, @option_hash[:c] = false }
+    # 残りのコマンドライン引数を配列に入れる
+    begin
+      @files = opt.parse(argv_array)
+      # オプションが指定されていない場合、デフォルトで行数、単語数、バイト数を表示する
+      if @option_hash == { l: false, w: false, c: false, m: false }
+        @option_hash = { l: true, w: true, c: true }
+      end
+    # 無効なオプションが指定されていた場合
+    rescue => e
+      @answer_string += "wc: illegal option -- #{e.args[0][1]}\nusage: wc [-clmw] [file ...]\n"
     end
-    # ファイルを開いて読み込み、オプションを適用し文字列を得る
-    answer_string = file_io
-    # 答えの文字列を表示し返す
-    print answer_string
-    answer_string
   end
 
   # ファイルを開いて読み込み、オプションを適用し閉じる
-  def file_io
+  def count_file(file)
+    buf = ""
     begin
-      File.open(@files[0], "r") do |file|
-        # EOFまでの全てのデータをバイナリ読み込みメソッドとして読み込み、その文字列を返す
-        @buf = file.read(nil)
+      File.open(file, "r") do |f|
+        # EOFまでの全てのデータを読み込み、その文字列を返す
+        buf = f.read
       end
     # 例外処理
-    rescue Errno::ENOENT => e
-      "wc: #{@files[0]}: open: No such file or directory\n"
-    rescue Errno::EISDIR => e
-      "wc: #{@files[0]}: read: Is a directory\n"
+    rescue Errno::ENOENT
+      @answer_string += "wc: #{file}: open: No such file or directory\n"
+      return false
+    rescue Errno::EISDIR
+      @answer_string += "wc: #{file}: read: Is a directory\n"
+      return false
+    # 正常にファイルを開ければ
     else
-      # オプションを適用し、文字列を返す
-      "#{apply_l_option}#{apply_w_option}#{apply_c_option} #{@files[0]}\n"
+      # オプションを適用し、文字列を得る
+      apply_l_option(buf) if @option_hash[:l]
+      apply_w_option(buf) if @option_hash[:w]
+      apply_c_option(buf) if @option_hash[:c]
+      apply_m_option(buf) if @option_hash[:m]
+      return true
     end
   end
 
-  # l オプションを適用し、1文字のスペースと７桁の数字で返す
-  def apply_l_option
-    if @option_hash[:l]
-      # 行数を数える
-      n_lines = @buf.count("\n")
-      n_lines.to_s.rjust(8)
-    end
+  # l オプションを適用し、1文字のスペースと７桁の数字からなる文字列を得る
+  def apply_l_option(buf)
+    # 行数を数える（@bufが不正なバイト列を含む場合?に置き換える）
+    n_line = buf.scrub.count("\n")
+    @answer_string += n_line.to_s.rjust(8)
+    @total_hash[:l] += n_line
   end
 
-  # w オプションを適用し、1文字のスペースと７桁の数字で返す
-  def apply_w_option
-    if @option_hash[:w]
-      # 単語数を数える
-      n_word = @buf.split.count
-      n_word.to_s.rjust(8)
-    end
+  # w オプションを適用し、1文字のスペースと７桁の数字からなる文字列を得る
+  def apply_w_option(buf)
+    # 単語数を数える（@bufが不正なバイト列を含む場合?に置き換える）
+    # 空白文字（半角スペース、タブ、改行文字）で区切る
+    #@buf.bytes
+    n_word = buf.scrub.split.count
+    @answer_string += n_word.to_s.rjust(8)
+    @total_hash[:w] += n_word
   end
 
-  # c オプションを適用し、1文字のスペースと７桁の数字で返す
-  def apply_c_option
-    if @option_hash[:c]
-      # バイト数を数える
-      n_bytesize = @buf.bytesize
-      n_bytesize.to_s.rjust(8)
-    end
+  # c オプションを適用し、1文字のスペースと７桁の数字からなる文字列を得る
+  def apply_c_option(buf)
+    # バイト数を数える
+    n_bytesize = buf.bytesize
+    @answer_string += n_bytesize.to_s.rjust(8)
+    @total_hash[:c] += n_bytesize
+  end
+
+  # m オプションを適用し、1文字のスペースと７桁の数字からなる文字列を得る
+  def apply_m_option(buf)
+    # 文字数を数える（マルチバイト文字1文字も1文字として返す）
+    n_size = buf.size
+    @answer_string += n_size.to_s.rjust(8)
+    @total_hash[:m] += n_size
   end
 end
 
 if __FILE__ == $PROGRAM_NAME
-  wc = Wc.new(ARGV)
-  wc.wc
+  wc = Wc.new
+  wc.wc(ARGV)
 end
